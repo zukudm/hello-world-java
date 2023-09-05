9e832f031bda1b8a18ca4ea1383d56e7a3649615
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-05 18:34:15 +0300

    Update HelloWorld.java 18:34

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Original version has been returned3");
+    System.out.println("Hello world! Original version has been returned4");
   }
 }
