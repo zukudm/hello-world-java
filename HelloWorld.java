1864f08303e9f1f764ea1d4cd545b7d374972f55
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-05 17:00:07 +0300

    Update 2 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Hello world again");
+    System.out.println("Hello world! Hello world again and finally hello the last time");
   }
 }
