de68cdc2e25823b08535598a062207769ba690ab
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-06 17:06:18 +0300

    Update HelloWorld.java branch2 update 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Text for the another branch, for tests branch2 update 1");
+    System.out.println("Hello world! Text for the another branch, for tests branch2 update 2");
   }
 }
