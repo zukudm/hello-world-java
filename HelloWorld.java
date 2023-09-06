05a1d4986c99507ccc51da1a14cdc78d44126d6b
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-06 18:20:24 +0300

    Update HelloWorld.java API new try number 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! getting results via API new try number 2");
+    System.out.println("Hello world! getting results via API new try number 3");
   }
 }
