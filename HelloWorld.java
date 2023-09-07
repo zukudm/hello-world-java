244b5c37525968c847d7e21c708a0c5e14e2b891
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-07 10:40:14 +0300

    Update HelloWorld.java API number 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! getting results via API number 1 ");
+    System.out.println("Hello world! getting results via API number 2 ");
   }
 }
