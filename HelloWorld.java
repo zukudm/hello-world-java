7abd34dd05225916e4e076c673241ea5dae1b4db
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-07 09:57:23 +0300

    Update HelloWorld.java  API with a token try number 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! getting results via API with a token try number 1");
+    System.out.println("Hello world! getting results via API with a token try number 2");
   }
 }
