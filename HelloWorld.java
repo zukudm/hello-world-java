8a84b075f3d4eec25469c9b40a72a695be2d6d55
zukudm <100283102+zukudm@users.noreply.github.com>
2023-09-07 12:15:49 +0300

    Update HelloWorld.java API 12:15

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! getting results via API 12:12");
+    System.out.println("Hello world! getting results via API 12:15");
   }
 }
